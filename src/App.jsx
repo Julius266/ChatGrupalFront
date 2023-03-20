b68d5661337b5d79116dae96a9a4bc330fca01c8
Edwin Rozo Gómez <erozog125@gmail.com>
2023-03-20 13:46:20 -0500

    finished client chat

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
-import io from 'socket.io-client';
 import './App.css'
+import { ChatClient } from './components/chat-client/ChatClient';
 
-const socket = io('http://localhost:4000');
 
 function App() {  
 
   return (
-    <div className="App">
-      <h1>Hello world - Chat</h1>
+    <div className="App"> 
+      <h1>Chat-io</h1>     
+      <ChatClient />
     </div>
   )
 }
