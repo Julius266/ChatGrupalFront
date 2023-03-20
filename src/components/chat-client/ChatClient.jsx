10f3ab0c804c675545c5c64fec651f4f4e74af9e
Edwin Rozo Gómez <erozog125@gmail.com>
2023-03-20 14:24:05 -0500

    Change on URL

--- src/components/chat-client/ChatClient.jsx
+++ src/components/chat-client/ChatClient.jsx
@@ -2,8 +2,8 @@ import React, { useState, useEffect } from 'react';
 import './chatClient.css';
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:4000');
-// const socket = io('*');
+// const socket = io('http://localhost:4000');
+const socket = io('https://chat-server-edwin-dev.onrender.com/');
 
 export const ChatClient = () => {
 
